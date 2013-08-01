8a132012d129bf0922285d2fb72653061d324bc7
ashiguruma <tom.loudon@axisto.com>
2013-08-01 22:48:23 +0100

    Added stub gaee.js object

---
+++ gaee.js
@@ -0,0 +1,20 @@
+/**
+* GAEE.js v0.0.1
+* 
+* MIT licensed
+*
+* Copyright (C) 2013 Thomas Loudon
+*/
+
+'use strict';
+
+(function () {
+
+    var Gaee = function (account, options) {
+        this.account = account;
+        this.options = options;
+    };
+
+    return Gaee;
+
+})();
