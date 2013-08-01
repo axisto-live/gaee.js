13e6924200c1a1787b41060fc70a594b0ae80778
ashiguruma <tom.loudon@axisto.com>
2013-08-01 21:28:16 +0100

    Added Gruntfile with jshint task

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['Gruntfile.js']
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
+};
