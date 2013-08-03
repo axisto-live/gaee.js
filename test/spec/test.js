fe9b09c2e4aa11eb87e542ef89d09415d43c8db8
ashiguruma <tom.loudon@axisto.com>
2013-08-03 11:40:32 +0100

    Updated test to wrap isValidAccount in a describe

--- test/spec/test.js
+++ test/spec/test.js
@@ -14,27 +14,31 @@
       delete this.gaee;
     });
 
-    it('should only accept a valid GA tracking code', function () {
+    describe('#isValidAccount', function () {
 
-      var self = this;
+      it('should only accept a valid GA tracking code', function () {
 
-      var badCodes = [
-        'ua-12345-1', // lower case
-        'ua-12345-a', // non int ending
-        '1-12345-a', // int beginning
-        'UA-123-1', // too short
-        'UA-1234567891-1' // too long
-      ];
+        var self = this;
 
-      badCodes.forEach(function (badCode) {
+        var badCodes = [
+          'ua-12345-1', // lower case
+          'ua-12345-a', // non int ending
+          '1-12345-a', // int beginning
+          'UA-123-1', // too short
+          'UA-1234567891-1' // too long
+        ];
+
+        badCodes.forEach(function (badCode) {
+          expect(function () {
+            self.gaee.isValidAccount(badCode);
+          }).to.throw(Error);
+        });
+
         expect(function () {
-          self.gaee.isValidAccount(badCode);
-        }).to.throw(Error);
-      });
+          self.gaee.isValidAccount('UA-1234-1');
+        }).not.to.throw(Error);
 
-      expect(function () {
-        self.gaee.isValidAccount('UA-1234-1');
-      }).not.to.throw(Error);
+      });
 
     });
 
