be3a9e830352462a0a8f36173e516d1b11bef221
ashiguruma <tom.loudon@axisto.com>
2013-08-23 12:50:44 +0100

    Added NetStream monitoring provided by @ohmshari

--- gaee.jwplayer5.js
+++ gaee.jwplayer5.js
@@ -13,19 +13,59 @@ function JWPlayer5 (tracker, player) {
 
   Gaee.call(this, tracker);
 
+  this.timer = setInterval(function () {
+    self.monitorStream();
+  }, 1000);
+
   this.player = player;
 
-  this.player.onPlay(function () {
+  this.player.onReady(function () {
+    console.log('onReady');
+  }).onPlay(function () {
     self.onPlay();
+    console.log('onPlay');
   }).onPause(function () {
     self.onPause();
+    console.log('onPause');
   }).onIdle(function () {
     self.onIdle();
+    console.log('onIdle');
+  }).onError(function (error) {
+    console.log('onError', error.message);
   });
 
 }
 
 JWPlayer5.prototype = new Gaee;
+
+JWPlayer5.prototype.monitorStream = function () {
+  var metadata = this.player.getMeta();
+
+  if (metadata.code === 'NetStream.Play.Start') {
+    // Stream starting up
+    console.log('NetStream.Play.Start');
+  } else if (metadata.code === 'NetStream.Buffer.Full') {
+    // Stream playing
+    console.log('NetStream.Buffer.Full');
+  } else if (metadata.code === 'NetStream.Buffer.Flush') {
+    // Stream paused
+    console.log('NetStream.Buffer.Flush');
+  } else if (metadata.code === 'NetStream.Buffer.Empty') {
+    // Bandwidth problems
+    console.log('NetStream.Buffer.Empty');
+  } else if (metadata.code === 'NetConnection.Connect.NetworkChange') {
+    // Network switch i.e. wired to wireless
+    console.log('NetConnection.Connect.NetworkChange');
+  } else if (metadata.code === 'NetConnection.Connect.Closed') {
+    // Connection closed
+    console.log('NetConnection.Connect.Closed');
+  } else if (metadata.code === 'NetConnection.Connect.Failed') {
+    // No network connection
+    console.log('NetConnection.Connect.Failed');
+  }
+
+  return this;
+}
 
 JWPlayer5.prototype.onPlay = function () {
   this.send({
@@ -51,6 +91,16 @@ JWPlayer5.prototype.onIdle = function () {
   this.send({
     category: 'gaee.jwplayer5',
     action: 'idle',
+    label: this.player.config.streamer + this.player.config.file
+  });
+
+  return this;
+}
+
+JWPlayer5.prototype.onBufferEmpty = function () {
+  this.send({
+    category: 'gaee.jwplayer5',
+    action: 'buffer empty',
     label: this.player.config.streamer + this.player.config.file
   });
 
